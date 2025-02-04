6c03538f113a86c8fc6a8141907a7fdb25ba65cf
NoahP-K <noahpk@bu.edu>
2025-02-03 21:09:08 -0500

    Altered method of spacing photos to use alignments.

--- app/src/main/java/com/example/individualassignment_22/MainActivity.kt
+++ app/src/main/java/com/example/individualassignment_22/MainActivity.kt
@@ -5,26 +5,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,7 +47,6 @@ fun makePhoto(caption: String, photo: Int){
             .width(width = 200.dp)
             .fillMaxHeight()
             .padding(
-                horizontal = 12.dp,
                 vertical = 12.dp)
     ){
         Image(
@@ -73,7 +67,7 @@ fun makePhoto(caption: String, photo: Int){
 //Argument is an array of pairs which each contain a photo and its caption.
 @Composable
 fun photoRow(photos: Array<Pair<Int, String>>){
-    Row(
+    Row(horizontalArrangement = Arrangement.SpaceEvenly,
         modifier = Modifier
             .fillMaxWidth()
             .height(height = 280.dp)
